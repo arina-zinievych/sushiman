3053b0abff7a7a3af5d9195540b0bb28ce349838
Arina Zinievych <120561883+arina-zinievych@users.noreply.github.com>
2023-12-09 20:27:26 +0200

    install motion library and add animations for sections

--- js/script.js
+++ js/script.js
@@ -5,14 +5,112 @@ import sushi12 from '../assets/sushi-12.png'
 import sushi11 from '../assets/sushi-11.png'
 import sushi10 from '../assets/sushi-10.png'
 
-import AOS from "aos";
-import "aos/dist/aos.css";
+import { animate, inView, stagger } from 'motion'
 
-// init AOS animation
-AOS.init({
-    duration: 1000,
-    offset: 100,
+
+inView('.header__nav', ({ target }) => {
+    animate(
+        target.querySelector('.header__logo h4'), {
+            opacity: ['0', '1'],
+            y: ['-50px', '0']
+        },
+        { duration: 0.4, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.header__menu'), {
+            opacity: ['0', '1'],
+            y: ['-50px', '0']
+        },
+        { duration: 0.4, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.header__menu-mobile'), {
+            opacity: ['0', '1'],
+            y: ['-50px', '0']
+        },
+        { duration: 0.4, easing: 'linear' }
+    )
 });
+
+inView('section.hero', ({ target }) => {
+    animate(
+        target.querySelector('.hero-image img'), {
+            opacity: ['0', '1'],
+            y: ['400px', '0']
+        },
+        { duration: 0.4, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.hero-image h2'), {
+            opacity: ['0', '1'],
+            y: ['400px', '0']
+        },
+        { duration: 0.6, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.hero-content-info'), {
+            opacity: ['0', '1'],
+            x: ['-400px', '0']
+        },
+        { duration: 0.4, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.hero-content__testimonial'), {
+            opacity: ['0', '1'],
+            y: ['400px', '0']
+        },
+        { duration: 0.6, easing: 'linear' }
+    )
+});
+
+inView('section.about-us', ({ target }) => {
+    animate(
+        target.querySelector('.about-us__image-sushi3 img'), {
+            opacity: ['0', '1'],
+            x: ['-400px', '0']
+        },
+        { duration: 0.6, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.about-us__image-sushi2 img'), {
+            opacity: ['0', '1'],
+            x: ['-400px', '0']
+        },
+        { duration: 0.6, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.about-us__content'), {
+            opacity: ['0', '1'],
+            x: ['400px', '0']
+        },
+        { duration: 0.6, easing: 'linear' }
+    )
+});
+
+inView('section.popular-foods', ({ target }) => {
+    animate(
+        target.querySelector('.popular-foods__title'), {
+            opacity: ['0', '1'],
+            y: ['-200px', '0']
+        },
+        { duration: 0.8, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.popular-foods__filters'), {
+            opacity: ['0', '1'],
+            y: ['80px', '0']
+        },
+        { duration: 0.8, easing: 'linear' }
+    ),
+    animate(
+        target.querySelector('.popular-foods__catalogue'), {
+            opacity: ['0', '1'],
+            y: ['400px', '0']
+        },
+        { duration: 0.8, easing: 'linear' }
+    )
+});
+
 
 const trendingSushis = [
     'Make Sushi',
