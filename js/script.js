16f9305bbda050c57085ab0011a416ac06b3d1fb
Arina Zinievych <120561883+arina-zinievych@users.noreply.github.com>
2023-12-13 14:26:17 +0200

    clean script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-// import images as relative image path won't work with vite/vercel.
-import check from '../assets/check.svg'
-import star from '../assets/star.svg'
-import sushi12 from '../assets/sushi-12.png'
-import sushi11 from '../assets/sushi-11.png'
-import sushi10 from '../assets/sushi-10.png'
-
-import { animate, inView, stagger } from 'motion'
+import { animate, inView } from 'motion';
 
 
 inView('.header__nav', ({ target }) => {
@@ -172,47 +165,3 @@ inView('section.subscription', ({ target }) => {
         { duration: 1, easing: 'linear' }
     )
 });
-
-
-const trendingSushis = [
-    'Make Sushi',
-    'Nigiri Sushi',
-    'Oshizushi',
-    'Temaki Sushi',
-    'Uramaki Sushi',
-    'Inari Sushi'
-];
-
-const trendingDrinks = [
-    "Oruncha",
-    "Ofukucha",
-    "Sakura Tea",
-    "Kombu-cha",
-    "Aojiru",
-    "Mugicha",
-]
-
-const cards = [
-    {
-        imgSrc: sushi12,
-        alt: "sushi-12",
-        title: "Chezu Sushi",
-        rating: "4.8",
-        price: "$21.00"
-    },
-    {
-        imgSrc: sushi11,
-        alt: "sushi-11",
-        title: "Originale Sushi",
-        rating: "4.8",
-        price: "$21.00",
-        active: true
-    },
-    {
-        imgSrc: sushi10,
-        alt: "sushi-10",
-        title: "Ramen Legendo",
-        rating: "4.8",
-        price: "$21.00"
-    }
-];
